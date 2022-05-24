47c3d481b44f96161f25796e258e5a26822623b0
Carlos André <cagd2002@gmail.com>
2022-05-24 15:58:43 -0300

    feat: Add edit resume page

---
+++ src/pages/EditResume/index.tsx
@@ -0,0 +1,47 @@
+import {
+  Accordion, Button, Container, Heading, Text,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+
+import { Header } from '../../components/Header';
+import { PersonalInformationSection } from './PersonalInformationSection';
+
+export function EditResume() {
+  const { user, logOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <Container maxW="870px">
+      <Header />
+      <Heading as="h2" my={8}>Meu Currículo</Heading>
+
+      <Accordion allowMultiple>
+        <PersonalInformationSection />
+      </Accordion>
+
+      <Text>
+        token
+        {' '}
+        {user?.token}
+      </Text>
+      <Text>
+        id
+        {' '}
+        {user?.id}
+      </Text>
+      <Text>
+        username
+        {' '}
+        {user?.username}
+      </Text>
+      <Button onClick={() => {
+        logOutUser();
+        navigate('/');
+      }}
+      >
+        Deslogar
+      </Button>
+    </Container>
+  );
+}
